a80607723d201e2af8fab91e121a99501465cb83
dpenedo <dpenedo@protonmail.com>
2025-01-08 18:01:12 -0300

    filtros funcionando por separado

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.db.models import query
 from django.views import generic
 from .models import Pieza, TipoDePieza
 
@@ -8,11 +9,22 @@ class HomepageView(generic.ListView):
     context_object_name = "piezas"
 
     def get_queryset(self):
-        return Pieza.objects.all()
+        queryset = Pieza.objects.all()
+
+        autora = self.request.GET.get("autora")
+        if autora:
+            queryset = queryset.filter(autora=autora)
+
+        tipo_id = self.request.GET.get("tipo")
+        if tipo_id:
+            queryset = queryset.filter(tipo_id=tipo_id)
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["tipos_de_pieza"] = TipoDePieza.objects.all()
+        context["autoras"] = Pieza.AUTHORS
         return context
 
 
