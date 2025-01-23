381a05af2d111e450b568268fa70c99e5a7d1541
dpenedo <dpenedo@protonmail.com>
2025-01-23 17:36:05 -0300

    mostrar carrito funcionando

--- products/views.py
+++ products/views.py
@@ -47,9 +47,13 @@ class CartView(generic.ListView):
     context_object_name = "piezas"
 
     def get_queryset(self):
-        shopping_cart_list = self.request.GET.get("shopping-cart")
+        shopping_cart_list = self.request.GET.getlist("shopping-cart")
         if shopping_cart_list:
-            return Pieza.objects.filter(id__in=shopping_cart_list)
+            try:
+                shopping_cart_list = [int(pid) for pid in shopping_cart_list]
+                return Pieza.objects.filter(id__in=shopping_cart_list)
+            except ValueError:
+                return Pieza.objects.none()
         return Pieza.objects.none()
 
 
